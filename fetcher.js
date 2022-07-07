456c9798543980730809c29667781e595104e1c7
Wesley Corner <wescorner@gmail.com>
2022-07-07 00:37:44 +0000

    now works with all edge cases: invalid file path, invalid url, non-200 response, and if file already exists questions user to replace

--- fetcher.js
+++ fetcher.js
@@ -1,15 +1,51 @@
 const request = require("request");
 const fs = require("fs");
+const readline = require("readline");
 
 const URL = process.argv[2];
 const FILE_PATH = process.argv[3];
 
 request(URL, (error, response, body) => {
-	console.log("Error: ", error);
-	console.log("statusCode: ", response && response.statusCode);
-	fs.appendFile(FILE_PATH, body, (err) => {
+	if (error) {
+		"Error: ", error;
+		console.log("INVALID URL!");
+		return error;
+	} else if (response.statusCode !== 200) {
+		return console.log("Status Code: ", response.statusCode);
+	} else {
+		fs.access(FILE_PATH, (err) =>
+			err ? addFile(FILE_PATH, body) : replaceFile(FILE_PATH, body)
+		);
+	}
+});
+
+const addFile = function (path, body) {
+	fs.appendFile(path, body, (err) => {
 		if (err) console.log(err);
 		else
 			console.log(`Downloaded and saved ${body.length} bytes to ${FILE_PATH}`);
 	});
-});
+};
+
+const replaceFile = function (path, body) {
+	const rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout,
+	});
+	console.log("File already exists. Would you like to override? (y + Enter)");
+	rl.on("line", (input) => {
+		if (input === "y") {
+			fs.writeFile(path, body, (err) => {
+				if (err) console.log(err);
+				else {
+					console.log(
+						`Downloaded and saved ${body.length} bytes to ${FILE_PATH}`
+					);
+					rl.close();
+				}
+			});
+		} else {
+			rl.close();
+		}
+	});
+};
